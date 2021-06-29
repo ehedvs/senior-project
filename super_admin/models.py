5c2b4c66de96efe060689ac7295d73e1ce9981a5
ehedvs <ehedvs2021@gmail.com>
2021-06-29 05:18:24 -0700

    interface for super admin

--- super_admin/models.py
+++ super_admin/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_no1 = PhoneNumberField()
+    phone_no2 = PhoneNumberField(blank=True)
+    fax_no = PhoneNumberField()
+    website = models.URLField(max_length=200)
+    pob = models.PositiveSmallIntegerField()
+    city = models.CharField(max_length=200, blank=True)
+    logo = models.ImageField(upload_to="logos/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    
+
+
+
+
